87db535059d7940db7c6eac3bfc178feddc62c05
Jochen Meyvisch <jochen.meyvisch@buffl.onmicrosoft.com>
2025-02-20 22:36:52 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HeaderBlock from './components/HeaderBlock.vue'
+</script>
+
+<template>
+  <HeaderBlock title="FC Los Tigres" />
+  <header></header>
+
+  <main></main>
+</template>
+
+<style scoped>
+body {
+  background-color: #161616;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
